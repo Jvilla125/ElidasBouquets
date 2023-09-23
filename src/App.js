b60a9eac4b47ca4a89e84324a6755fc7cd31f47a
jeffvilla <jvilla125@gmail.com>
2023-09-22 19:52:16 -0700

    Edit UserProfilePageComponent to include input fields for users to update their profile information and password. Create new Routes for Admin in App.js and route paths

--- src/App.js
+++ src/App.js
@@ -48,9 +48,24 @@ function App() {
         {/* user protected routes */}
         {/* pass the admin prop to the ProtectedRoutesComponent */}
         {/* <Route element={<ProtectedRoutesComponent admin={false} />}  > */}
-          <Route path="/user" element={<UserProfilePage />} />
-          <Route path="/user/order-history" element={<UserOrdersPage />} />
+        <Route path="/user" element={<UserProfilePage />} />
+        <Route path="/user/order-history" element={<UserOrdersPage />} />
         {/* </Route> */}
+
+        {/* admin protected routes */}
+        {/* pass the admin prop to the ProtectedRoutesComponent */}
+        <Route element={<ProtectedRoutesComponent admin={true} />} >
+          <Route path="/admin/users" element={<AdminUsersPage />} />
+          <Route path="/admin/edit-user/:id" element={<AdminEditUserPage />} />
+          <Route path="/admin/products" element={<AdminProductsPage />} />
+          <Route path="/admin/create-new-product" element={<AdminCreateProductPage />} />
+          <Route path="/admin/edit-product/:id" element={<AdminEditProductPage />} />
+          <Route path="/admin/orders" element={<AdminOrdersPage />} />
+          <Route path="/admin/order-details/:id" element={<AdminOrderDetailsPage />} />
+          <Route path="/admin/analytics" element={<AdminAnalyticsPage />} />
+          <Route path="/admin/chats" element={<AdminChatsPage />} />
+        </Route>
+
       </Routes>
 
       <FooterComponent />
--- src/pages/user/components/UserProfilePageComponent.js
+++ src/pages/user/components/UserProfilePageComponent.js
@@ -7,9 +7,9 @@ const UserProfilePageComponent = () => {
         <>
             <div className="grid mt-6 md:mb-10 grid-cols-1 lg:grid-cols-8">
                 {/* start of first div */}
-                <UserLinksComponent/>
+                <UserLinksComponent />
                 {/* start of second div */}
-                <div className="bg-blue-200 w-full row-start-2 col-start-4 col-span-4">
+                <div className=" w-full row-start-2 col-start-4 col-span-4">
                     <div class="bg-white overflow-hidden shadow rounded-lg border">
                         <div class="px-4 py-5 sm:px-6">
                             <h3 class="text-lg leading-6 font-medium text-gray-900">
@@ -19,42 +19,88 @@ const UserProfilePageComponent = () => {
                                 This is some information about the user.
                             </p>
                         </div>
-                        <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
-                            <dl class="sm:divide-y sm:divide-gray-200">
-                                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt class="text-sm font-medium text-gray-500">
-                                        Full name
-                                    </dt>
-                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                        John Doe
-                                    </dd>
+                        <form>
+                            <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+                                <dl class="sm:divide-y sm:divide-gray-200">
+                                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                        <label className="text-sm font-medium text-gray-500"
+                                            for="firstName">
+                                            First Name
+                                        </label>
+                                        <input className="text-sm text-gray-900 w-full leading-tight rounded"
+                                            id="firstName"
+                                            type="text"
+                                            placeholder="First Name" />
+                                    </div>
+                                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                        <label className="text-sm font-medium text-gray-500"
+                                            for="lastName">
+                                            Last Name
+                                        </label>
+                                        <input className="text-sm text-gray-900 w-full leading-tight rounded"
+                                            id="lastName"
+                                            type="text"
+                                            placeholder="Last Name" />
+                                    </div>
+                                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+
+                                        <label className="text-sm font-medium text-gray-500"
+                                            for="phone">
+                                            Phone Number
+                                        </label>
+                                        <input className="text-sm text-gray-900 w-full leading-tight rounded"
+                                            id="phone"
+                                            type="tel"
+                                            placeholder="Phone Number" />
+                                    </div>
+                                    <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                        <label className="text-sm font-medium text-gray-500"
+                                            for="email">
+                                            Email Address
+                                        </label>
+                                        <input className="text-sm text-gray-900 w-full leading-tight rounded"
+                                            id="email"
+                                            type="email"
+                                            placeholder="Email Address" />
+                                    </div>
+                                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Update</button>
+                                </dl>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className="mt-4 row-start-4 col-start-4 col-span-4 mb-8">
+                    <div class="bg-white overflow-hidden shadow rounded-lg border">
+                        <div class="px-4 py-5 sm:px-6">
+                            <h3 class="text-lg leading-6 font-medium text-gray-900">
+                                Update Password
+                            </h3>
+                            <form>
+                                <div className="grid md:grid-cols-2 md:gap-6 mt-4">
+                                    <div className="mb-4">
+                                        <label className="block text-gray-700 text-sm font-bold mb-2 text-xl"
+                                            for="password">
+                                            Password
+                                        </label>
+                                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            id="password"
+                                            type="password"
+                                            placeholder="Password" />
+                                    </div>
+                                    <div className="mb-4">
+                                        <label className="block text-gray-700 text-sm font-bold mb-2 text-xl"
+                                            for="confirmPassword">
+                                            Confirm Password
+                                        </label>
+                                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                            id="confirmPassword"
+                                            type="password"
+                                            placeholder="Confirm Password" />
+                                    </div>
                                 </div>
-                                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt class="text-sm font-medium text-gray-500">
-                                        Email address
-                                    </dt>
-                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                        johndoe@example.com
-                                    </dd>
-                                </div>
-                                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt class="text-sm font-medium text-gray-500">
-                                        Phone number
-                                    </dt>
-                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                        (123) 456-7890
-                                    </dd>
-                                </div>
-                                <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt class="text-sm font-medium text-gray-500">
-                                        Address
-                                    </dt>
-                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                        123 Main St<br/>
-                                            Anytown, USA 12345
-                                    </dd>
-                                </div>
-                            </dl>
+                                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Update</button>
+
+                            </form>
                         </div>
                     </div>
                 </div>
