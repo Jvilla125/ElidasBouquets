ce9076a3a1fb78e6bed0132399c56d067dc4a61d
jeffvilla <jvilla125@gmail.com>
2023-09-22 21:05:16 -0700

    Create AdminLinksComponent and import to AdminUserPageComponent.js

--- src/pages/admin/AdminUsersPage.js
+++ src/pages/admin/AdminUsersPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 import AdminUsersPageComponent from "./components/AdminUsersPageComponent";
 
 const AdminUsersPage = () => {
--- src/pages/admin/components/AdminUsersPageComponent.js
+++ src/pages/admin/components/AdminUsersPageComponent.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+import AdminLinksComponent from "../../../components/admin/AdminLinksComponent";
+
 const AdminUsersPageComponent = () => {
     return (
         <>
-            <h1>Components page</h1>
+            <div className="grid mt-6 md:mb-10 grid-cols-1 lg:grid-cols-10">
+                <AdminLinksComponent />
+                <div className=" w-full row-start-2 col-start-4 col-span-6">
+                    <h1>Components page</h1>
+                </div>
+            </div>
         </>
     )
 }
