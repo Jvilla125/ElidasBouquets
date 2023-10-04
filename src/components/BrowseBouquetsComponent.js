23da4e4963405a1332f1081337430a3c42c21c16
jeffvilla <jvilla125@gmail.com>
2023-10-03 20:45:10 -0700

    Update the headerComponent to be more responsive when on a smaller screen

--- src/components/BrowseBouquetsComponent.js
+++ src/components/BrowseBouquetsComponent.js
@@ -19,30 +19,30 @@ const BrowseBouquets = () => {
         {
             name: "bouquet4",
             price: "$59"
-        }
+        },
+
     ]
 
     return (
         <>
-            <div className="flex flex-col bg-slate-300 bg-opacity-50 border border-gray-900 rounded-lg ml-8 mr-8 dark:bg-gray-200 dark:border-gray-900 ">
-                
-                <div className="flex flex-row items-end mb-4 ml-8">
-                    <h1 className="font-semibold text-3xl mr-4 ">
-                        Browse Bouquets
+            <div className=" flex flex-col w-11/12 mx-auto bg-slate-300 bg-opacity-50 border border-gray-900 rounded-lg dark:bg-gray-200 dark:border-gray-900 ">
+                <div className="flex flex-row justify-center items-center items-end ">
+                    <h1 className="font-semibold text-4xl ml-4 mr-4 ">
+                        Browse All Bouquets
                     </h1>
-                    <p>See all Flowers -</p>
                 </div>
-                <div className="flex flex-row flex-wrap justify-center items-center space-x-2 ">
+                <div className="flex flex-row flex-wrap justify-center items-center space-x-2">
                     {bouquets.map((item, idx) => (
-                        <div className="w-full max-w-xs ">
+                        <div key={idx} className="w-full max-w-xs sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-2">
                             <a href="#">
                                 <img className=" rounded-xl" src="/images/BouquetOne.jpg" alt="product image" />
                             </a>
-                            <div className="px-5 pb-5 flex flex-col items-center">
+                            <div className=" pb-5 flex flex-col ml-4">
+                            <span className="text-xl font-bold text-gray-900 dark:text-white">{item.price}</span>
                                 <a href="#">
-                                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{item.name}</h5>
+                                    <h5 className="text-xl  tracking-tight text-gray-900 dark:text-white">{item.name}</h5>
                                 </a>
-                                <span className="text-xl font-bold text-gray-900 dark:text-white">{item.price}</span>
+                                
                                 <a href="#">Buy Now -</a>
                             </div>
                         </div>
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -36,8 +36,7 @@ const HeaderComponent = () => {
                 {/* User Profile Navbar */}
                 <nav className="">
                     <div className="container mx-auto px-4">
-                        <ul className="flex justify-between items-center py-2">
-                            <li><a href="/profile" className="hover:text-blue-300">My Profile</a></li>
+                        <ul className="flex justify-center items-center py-2">
                             <Link to="/">
                                 <li>
                                     <div className="flex flex-col items-center flex-shrink-0 ">
@@ -57,7 +56,6 @@ const HeaderComponent = () => {
                                     </div>
                                 </li>
                             </Link>
-                            <li><a href="/logout" className="hover:text-blue-300">Logout</a></li>
                         </ul>
                     </div>
                 </nav>
@@ -65,22 +63,41 @@ const HeaderComponent = () => {
                 {/* Secondary Actions Navbar */}
                 <nav className="text-black">
                     <div className="container mx-auto px-4">
-                        <ul className="flex justify-between items-center py-2">
-                            <Link to="/" className="block mt-4 text-xl lg:inline-block lg:mt-0 hover:text-blue-500 mr-4">
-                                Home
-                            </Link>
-                            <Link to="shopAll" className="block mt-4 text-xl lg:inline-block lg:mt-0 hover:text-blue-500 mr-4">
-                                Shop All
-                            </Link>
-
-                            <a href="#responsive-header" className="block mt-4 text-xl lg:inline-block lg:mt-0 hover:text-blue-500 mr-4">
-                                Occasions
-                            </a>
-                            <Link to="/about" className="block mt-4 text-xl lg:inline-block lg:mt-0 hover:text-blue-500">
-                                About us
-                            </Link>
+                        <ul className="flex flex-col lg:flex-row justify-between items-center py-2">
+                            <li className="mb-4 lg:mb-0">
+                                <Link to="/" className="text-xl lg:text-base lg:inline-block hover:text-blue-500 mr-4">
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="mb-4 lg:mb-0">
+                                <Link to="shopAll" className="text-xl lg:text-base lg:inline-block hover:text-blue-500 mr-4">
+                                    Shop All
+                                </Link>
+                            </li>
+                            <li className="mb-4 lg:mb-0">
+                                <div className="dropdown inline-block relative">
+                                    <button className="py-2 px-4 rounded inline-flex items-center">
+                                        <span className="text-xl lg:text-base lg:inline-block hover:text-blue-500">Occasions</span>
+                                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /> </svg>
+                                    </button>
+                                    <ul className="dropdown-menu absolute grid grid-cols-2 gap-4 hidden text-gray-700 pt-1">
+                                        <li className=""><a className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Anniversary</a></li>
+                                        <li className=""><a className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Birthday</a></li>
+                                        <li className=""><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Graduation</a></li>
+                                        <li className=""><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Mother's Day</a></li>
+                                        <li className=""><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Get Well Soon</a></li>
+                                        <li className=""><a className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Just Because</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li className="mb-4 lg:mb-0">
+                                <Link to="/about" className="text-xl lg:text-base lg:inline-block hover:text-blue-500">
+                                    About us
+                                </Link>
+                            </li>
                         </ul>
                     </div>
+                    <hr className="h-px bg-green-200 border-1 dark:bg-green-700"></hr>
                 </nav>
             </header>
         </>
