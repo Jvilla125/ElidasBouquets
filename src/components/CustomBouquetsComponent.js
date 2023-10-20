2f2a117fcbfd3f4d80b23b05a85b2759b0c485a9
jeffvilla <jvilla125@gmail.com>
2023-10-20 09:03:38 -0700

    update CustomBouquetsComponent to have the second div have a horizontal scroll and be responsive to the window size

--- src/components/CustomBouquetsComponent.js
+++ src/components/CustomBouquetsComponent.js
@@ -38,25 +38,35 @@ const CustomBouquetsComponent = () => {
 
     return (
         <>
-            <div className="flex-grow border-t border-gray-900"></div>
+            <div className="flex-grow border-t border-gray-900 pt-5"></div>
+            <div className="containerimg flex flex-col sm:w-7/12 w-11/12 mx-auto bg-slate-300 bg-opacity-50 border border-gray-900 rounded-lg dark:bg-gray-200 dark:border-gray-900  ">
+                <img src="/images/custom.avif" />
+                <div className="centered text-white sm:text-7xl  text-5xl"> Elida's Bouquets are customizable!</div>
+            </div>
             <div className="relative flex py-5 items-center">
+
                 <div className="flex-grow border-t ml-24 border-gray-900"></div>
                 <span className="flex-shrink mx-4 text-black text-5xl" >Check out these Custom Made Bouquets!</span>
                 <div className="flex-grow border-t mr-24 border-gray-900"></div>
             </div>
             {/* Start of Occasion Cards Section */}
-           
-            <div className="w-full max-w-xl mx-auto">
 
-                <div className="w-full flex flex-row overflow-x-scroll snap-x snap-mandatory">
+            
+            <div className="w-11/12 mx-auto bg-slate-300 bg-opacity-50 border border-gray-900 rounded-lg dark:bg-gray-200 dark:border-gray-900 ">
+                <div className="flex flex-row sm:h-96 h-88 overflow-x-scroll snap-x snap-mandatory space-x-12  pt-2 mt-2 ">
                     {Occasions.map((item, idx) => {
                         return (
-                            <>
-                                <div id={idx} className="w-full flex-shrink-0 snap-start">
-                                    <img src={item.img} alt="images"/>
-                                </div>
-                            </>
-
+                            <div key={idx} className="p-2 flex-none flex-shrink-0 snap-start ">
+                                <img
+                                    src={item.img}
+                                    alt="images"
+                                    className="border border-black rounded-lg object-cover h-full sm:h-80 h-72 "
+                                /> 
+                                <p className="text-center">
+                                    {item.name}
+                                </p>
+                                
+                            </div>
                         );
                     })}
                 </div>
