ff7420ef718bf50cdf0351e0645539e463a20266
jeffvilla <jvilla125@gmail.com>
2023-10-28 23:14:46 -0700

    Update the pagination to be dynamic

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,27 @@
+const recordsPerPage = require("../config/pagination")
 const Product = require("../models/ProductModel")
 
-const getProducts = (req, res) => {
-    Product.create({name: "Second Bouquet"})
-    res.send("hanlding products now, leave")
+const getProducts = async (req, res, next) => {
+    try {
+        const pageNum = Number(req.query.pageNum) || 1
+        const totalProducts = await Product.countDocuments({})
+
+        // sory by name, price
+        let sort = {}
+        const sortOption = req.query.sort || ""
+        if (sortOption) {
+            let sortOpt = sortOption.split("_")
+            // example <option value="price_1">
+            // we separate price = sortOpt[0] and 1 = sortOpt[1]
+            sort = { [sortOpt[0]]: Number(sortOpt[1]) }
+            console.log(sort)
+        }
+
+        const products = await Product.find({}).skip(recordsPerPage * (pageNum - 1)).sort({ name: 1 }).limit(recordsPerPage)
+        res.json({ products, pageNum, paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage) })
+    } catch (error) {
+        next(error)
+    }
 }
 
 module.exports = getProducts
